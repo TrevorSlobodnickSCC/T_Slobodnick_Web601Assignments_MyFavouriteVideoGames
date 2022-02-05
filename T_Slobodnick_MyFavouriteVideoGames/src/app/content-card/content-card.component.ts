76548ecc930a37346d26f0ec4543b667d59d8e5c
TrevorSlobodnickSCC <W0740591@myscc.ca>
2022-02-05 18:44:03 -0500

    Assignment2

--- T_Slobodnick_MyFavouriteVideoGames/src/app/content-card/content-card.component.ts
+++ T_Slobodnick_MyFavouriteVideoGames/src/app/content-card/content-card.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { ContentList } from '../helper-files/content-list';
+import { Component, Input, OnInit } from '@angular/core';
+import { Content } from '../helper-files/content-interface';
 
 @Component({
   selector: 'app-content-card',
@@ -8,36 +8,17 @@ import { ContentList } from '../helper-files/content-list';
 })
 export class ContentCardComponent implements OnInit {
 
-  contentList = new ContentList();
+  @Input() content?: Content;
 
   constructor() {
-    this.contentList.add({
-      id: 1,
-      title: "The Elder Scrolls V: Skyrim",
-      description: "The Elder Scrolls V: Skyrim is an open-world action role-playing video game developed by Bethesda Game Studios and published by Bethesda Softworks.",
-      creator: "Bethesda Game Studios",
-      imgURL: "https://upload.wikimedia.org/wikipedia/en/1/15/The_Elder_Scrolls_V_Skyrim_cover.png",
-      type: "RPG",
-      tags: []
-    });
-    this.contentList.add({
-      id: 2,
-      title: "Monster Hunter: World",
-      description: "Monster Hunter: World is an action role-playing game developed and published by Capcom.",
-      creator: "Capcom",
-      imgURL: "https://upload.wikimedia.org/wikipedia/en/1/1b/Monster_Hunter_World_cover_art.jpg",
-      type: "RPG",
-      tags: []
-    });
-    this.contentList.add({
-      id: 3,
-      title: "Dark Souls III",
-      description: "Dark Souls III is a 2016 action role-playing video game developed by FromSoftware and published by Bandai Namco Entertainment.",
-      creator: "Havoc",
-      imgURL: "https://upload.wikimedia.org/wikipedia/en/b/bb/Dark_souls_3_cover_art.jpg",
-      type: "RPG",
-      tags: []
-    });
+   
+  }
+
+  onImgClick(){
+    if(this.content){
+      console.log(this.content.id)
+      console.log(this.content.title)
+    }
   }
 
   ngOnInit(): void {
