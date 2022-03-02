e508e700717f0fd84e2f229653ef3268afc72538
TrevorSlobodnickSCC <W0740591@myscc.ca>
2022-03-02 00:09:04 -0500

    A5 - almost there

--- T_Slobodnick_MyFavouriteVideoGames/src/app/content-list/content-list.component.ts
+++ T_Slobodnick_MyFavouriteVideoGames/src/app/content-list/content-list.component.ts
@@ -101,10 +101,13 @@ export class ContentListComponent implements OnInit {
     this.searchMsg = "Could not find title - \"" + this.titleSearch + "\"";
   }
 
-  addContentToList(newContent: Content) {
-    this.content.push(newContent);
-    this.content = [...this.content];
-    console.log("New content added successfully - " + newContent.title); //inside success function
+  addContentToList(newContent: Promise<Content>) {
+    newContent.then(value => {
+      this.content.push(value)
+      this.content = [...this.content];
+      console.log("New content added successfully - " + value.title);
+    })
+    .catch(v => console.log(v))
   }
 
 }
--- T_Slobodnick_MyFavouriteVideoGames/src/app/create-content/create-content.component.ts
+++ T_Slobodnick_MyFavouriteVideoGames/src/app/create-content/create-content.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { Content } from '../helper-files/content-interface';
 
 @Component({
@@ -8,21 +8,62 @@ import { Content } from '../helper-files/content-interface';
 })
 export class CreateContentComponent implements OnInit {
 
-  @Output() newGameEvent = new EventEmitter<Content>();
-  newContent?: Content;
-  constructor() { }
+  @Output() newGameEvent = new EventEmitter<Promise<Content>>();
+ 
+  @Input() idInp: string;
+  @Input() titleInp: string;
+  @Input() descriptionInp: string;
+  @Input() creatorInp: string;
+  @Input() imgURLInp: string;
+  @Input() typeInp: string;
+  @Input() tagsInp: string;
+
+  newContent?: Promise<Content>;
+  errorMsg = "";
+
+  constructor() { 
+    this.idInp = ""
+    this.titleInp = ""
+    this.descriptionInp = ""
+    this.creatorInp = ""
+    this.imgURLInp = ""
+    this.typeInp = ""
+    this.tagsInp = ""
+  }
 
   onFormSubmit(id: string, title: string, description: string, creator: string, imgURL: string, type: string, tags: string){
-    this.newContent = {
-      id: parseInt(id),
-      title: title,
-      description: description,
-      creator: creator,
-      imgURL: imgURL,
-      type: type,
-      tags: tags.split(",")
-    };
-    this.newGameEvent.emit(this.newContent);
+    let promise = new Promise<Content>((resolve, reject) => {
+      if(id && title && description && creator){
+        let nContent = {
+          id: parseInt(id),
+          title: title,
+          description: description,
+          creator: creator,
+          imgURL: imgURL,
+          type: type,
+          tags: tags.split(",")
+        };
+        this.errorMsg = "";
+        resolve(nContent);
+        this.resetForm()
+      }
+      else{
+        this.errorMsg = "Not all required fields contain values"
+        reject(this.errorMsg)
+      }
+    })
+    this.newContent = promise;
+    this.newGameEvent.emit(this.newContent); //causing odd behavior if not emitted
+  }
+
+  resetForm(){
+    this.idInp = ""
+    this.titleInp = ""
+    this.descriptionInp = ""
+    this.creatorInp = ""
+    this.imgURLInp = ""
+    this.typeInp = ""
+    this.tagsInp = ""
   }
 
   ngOnInit(): void {
